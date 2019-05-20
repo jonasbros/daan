fffd8da37c71966efa43748f43fac1ba0d39d3ec
Jonas Perez <00jonasperez@gmail.com>
2019-05-20 06:42:42 +0000

    added click event listener, waypointing

--- resources/js/mapbox/mapbox.js
+++ resources/js/mapbox/mapbox.js
@@ -27,35 +27,28 @@ var directionsRoutesControl = L.mapbox.directions.routesControl('routes', direct
 var directionsInstructionsControl = L.mapbox.directions.instructionsControl('instructions', directions)
     .addTo(map);
 
-directions.setOrigin(L.latLng(10.702153574060667, 122.97508255034256));
+let waypoints = [];
 
-let test = [
-    [10.702153574060667, 122.97508255034256],
-    [10.704683695515799, 122.96218648939896],
-    [10.698498199064593, 122.96218047077997],
-    [10.676485828610616, 122.95316569007701],
-    [10.67735036225555, 122.9509662786848],
-    [10.669509916754725, 122.94745560519357],
-    [10.670131973777483, 122.946093043014],
-    [10.666747646373937, 122.94360897938316],
-    [10.666136125896848, 122.94476769367759],
-    [10.661387948727644, 122.94232885810129],
-    [10.65861897610613, 122.94834919511607],
-    [10.660781587558162, 122.94943307879657],
-    [10.661804320144972, 122.94721220973224],
-    [10.676260613652854, 122.95316396145017],
-    [10.698479142266782, 122.96207151167681],
-    [10.704793695696443, 122.9623340017605],
-    [10.70222978276871, 122.97515510665687],
-]
-
-for(var i = 0; i < test.length; i++) {
-    directions.addWaypoint(i, L.latLng(test[i]));
+if( waypoints.length ) {
+    for(var i = 0; i < waypoints.length; i++) {
+        directions.addWaypoint(i, L.latLng(waypoints[i]));
+    }  
+    
+    directions.query();
 }
 
-directions.setDestination(L.latLng(10.702153574060667, 122.97508255034256));
+map.on('click', function(e) {
+    waypoints.push([e.latlng.lat, e.latlng.lng]);    
+    let waypointIndex = waypoints.length - 1;
+    let latLng = L.latLng(waypoints[waypointIndex]);
 
-directions.query();
-
+    if( waypointIndex == 0 ) {
+        directions.setOrigin(latLng);
+    } else {
+        directions.addWaypoint(waypointIndex, latLng);   
+        directions.setDestination(latLng);                
+    }
+    directions.query();
+});
 
 console.log(directions);
